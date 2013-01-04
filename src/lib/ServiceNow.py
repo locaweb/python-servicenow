83df63ca6c8debc0b14b6f3bd23ce4b04058518b
Juliano Martinez <juliano@martinez.io>
2013-01-04 20:37:59 -0200

    SUper Divertido

--- src/lib/ServiceNow.py
+++ src/lib/ServiceNow.py
@@ -1,17 +1,27 @@
+from servicenow import Utils
+
 class Base(object):
     __table__ = None
 
-    def __init__(self, connection)
-        self.connection = connection
+    def __init__(self, Connection):
+        self.Connection = Connection
 
-    def get(self, params):
-        return self.connection._get(self.__table__, params).json()
+    @Utils.cached()
+    def fetch_all(self, params):
+        return self.Connection._get(self.__table__, params).json()
+
+    @Utils.cached()
+    def fetch_one(self, params):
+        return self.Connection._get(self.__table__, params).json()['records'][0]
 
     def create(self, params):
-        return self.connection._post(self.__table__, params).json()
+        return self.Connection._post(self.__table__, params).json()
 
 class Server(Base):
     __table__ = 'cmdb_ci_server_list.do'
 
 class Incident(Base):
     __table__ = 'incident.do'
+
+class Group(Base):
+    __table__ = 'sys_user_group.do'
---
+++ src/lib/Utils.py
@@ -0,0 +1,25 @@
+import hashlib
+import json
+import redis
+
+from functools import wraps
+
+rd = redis.Redis()
+def cached(ttl=300):
+    def proxy(f):
+        @wraps(f)
+        def caching(*args, **kwargs):
+            _hash = "%s-%s" % (f.__name__, hashlib.md5("%s%s" % (
+                repr(args[1:]),
+                repr(kwargs)
+            )).hexdigest())
+            try:
+                cache = rd.get(_hash)
+                if not cache:
+                    cache = json.dumps(f(*args, **kwargs))
+                    rd.setex(_hash, cache, ttl)
+                return json.loads(cache)
+            except Exception, e:
+                return f(*args, **kwargs)
+        return caching
+    return proxy
