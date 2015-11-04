1d5bc28eba19487efd5922395f8207a89a1987ee
tommyle2048 <tommyle2048@gmail.com>
2015-11-04 13:22:37 -0800

    add Journal for reading the comments

--- src/servicenow/ServiceNow.py
+++ src/servicenow/ServiceNow.py
@@ -65,6 +65,9 @@ class Group(Base):
 class Incident(Base):
     __table__ = 'incident.do'
 
+class Journal(Base):
+    __table__ = 'sys_journal_field.do'
+
 class Problem(Base):
     __table__ = 'problem.do'
 
