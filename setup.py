ef869571a7727fa44d4f773da537859f18211170
Jose Ricardo Fernandes <jose.fernandes@locaweb.com.br>
2016-03-23 10:14:23 -0300

    add new version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author = 'Juliano Martinez',
     author_email = 'juliano.martinez@locaweb.com.br',
     name = 'servicenow',
-    version = '2.0.1',
+    version = '2.1.0',
     url = 'https://github.com/locaweb/python-servicenow',
     description = 'Python Library to interact with and manage the ServiceNow database',
     install_requires = ['requests','redis','SOAPpy'],
